a7f728b0ae5b13be7f9e2f08139d8068a67f66e3
Zhafron Adani Kautsar <zhafronadani@gmail.com>
2022-11-09 15:14:37 +0700

    google map picker can be dragged
    Signed-off-by: Zhafron Adani Kautsar <zhafronadani@gmail.com>

--- resources/views/backend/kos/edit.blade.php
+++ resources/views/backend/kos/edit.blade.php
@@ -202,7 +202,6 @@
 
     </script>
     <script
-        src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&libraries=places&callback=initialize"
-        async defer></script>
+        src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&libraries=places"></script>
     <script src="{{ asset('js/mapInput.js') }}"></script>
 @endpush
